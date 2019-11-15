45dbe8eb997a8e97f9b562fed5e7da4a5e523f85
Hui <jeffrey.x.hui@gmail.com>
2019-11-15 10:47:12 -0800

    modal with ternary data depending on which card implemented

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,15 +1,42 @@
-import React from "react";
-import {Card, CardBody, Col} from 'reactstrap';
+import React, {useState, useEffect} from "react";
+import {Card, CardBody, Col, Button, Modal, ModalHeader, ModalBody} from 'reactstrap';
+import InfoModal from './InfoModal';
 
 export default function CharacterCard({data}) {
+
+  const [useModal, setUseModal] = useState(false);
+
   return (
-    <Col>
-      <Card>
-        <CardBody>
-          <h5>{data.id}</h5>
-          <p>{data.name}</p>
-        </CardBody>
-      </Card>
-    </Col>
+    <>
+      <Col>
+        <Card>
+          <CardBody>
+            <h5>{data.id}</h5>
+            <p>{data.name}</p>
+            <Button onClick={() => setUseModal(!useModal)}>Info</Button>
+          </CardBody>
+        </Card>
+      </Col>
+      <Modal isOpen={useModal} onClick={() => setUseModal(!useModal)}>
+        <ModalHeader>
+          {data.name}
+        </ModalHeader>
+        <ModalBody>
+          <p>{data.id}</p>
+
+          {
+            data.type
+              ? <p>{data.type}</p>
+              : null
+          }
+          {
+            data.image
+              ? <img src={data.image} />
+              : null
+          }
+        </ModalBody>
+
+      </Modal>
+    </>
   )
 }
---
+++ src/components/InfoModal.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Modal, ModalHeader, ModalBody} from 'reactstrap';
+
+const InfoModal = props => {
+
+  return (
+    <Modal>
+      <ModalHeader>
+
+      </ModalHeader>
+      <ModalBody>
+
+      </ModalBody>
+
+    </Modal>
+  )
+}
+
+export default InfoModal;
